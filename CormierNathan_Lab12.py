40010177bea95ed393451e337b1aead4359eaca0
ncormie8 <ncormie8@uwo.ca>
2024-12-04 18:18:47 -0500

    Final edits for part 1a
    - Caught an error in my skip rows  value while working on the plot and corrected it (originally put 57 when I should have used 52)
    - Corrected the start_index and end_index values after correcting my original skip rows error
    - Added and modified comments to better describe my code
    - Added basic plotting code to reproduce Figure 5.1 from the textbook
    -

--- CormierNathan_Lab12.py
+++ CormierNathan_Lab12.py
@@ -7,29 +7,32 @@ from matplotlib import pyplot as plt
 
 # 1a)
 # Read the data from the file and use a method to select data only
-# from dates between 1981 and 1990. Plot the measurements to check
+# from dates from 1981 to 1990. Plot the measurements to check
 # that this selection reproduces figure 5.1 in the text.
 
 # reading the csv file from its raw string path and assigning it to csv_out
-csv_out = pd.read_csv(r'C:\Users\natha\Desktop\UWO\2024-2025\1st Semester\Physics 3926 - Computer simulations\Python\co2_mm_mlo.csv',skiprows=57)
+csv_out = pd.read_csv(r'C:\Users\natha\Desktop\UWO\2024-2025\1st Semester\Physics 3926 - Computer simulations\Python\co2_mm_mlo.csv',skiprows=52)
 
 # isolating only the needed data into easily manipulable np array
 csv_data_all = np.array(csv_out)
 
-# defining the index values to isolate data from 1981 to 1990 (inclusive) 
-start_index = 327-59
-end_index = 446-58
+# defining the index values to isolate data from 1981 to 1990 (exclusive) 
+start_index = 327-54
+end_index = 434-53
 
-# by reading the csv, I know I need only the data from rows start_index to end_index for 1981 to 1990
-csv_data_81_90 = csv_data_all[start_index:end_index,:]
-print(np.shape(csv_data_81_90))
+# by looking the csv, I know I need only the data from rows start_index to end_index for 1981 to 1990 (exclusive)
+csv_data_81_90ex = csv_data_all[start_index:end_index,:]
 
 # plotting the monthly average ppm of CO2 from 1981 to 1990
-t_axis = csv_data_81_90[0:119,2]
-y_axis_CO2 = csv_data_81_90[0:119,3]
+t_axis = csv_data_81_90ex[0:119,2]      # setting the time equal to the decimal dates
+y_axis_CO2 = csv_data_81_90ex[0:119,3]  # setting the ppm CO2 to the average measured values from the csv
 
-plt.plot()
-
+# reproducing the plot from Figure 5.1 in the textbook (with nicer formatting)
+plt.xlabel('Year')
+plt.ylabel('CO2 (ppm)')
+plt.title('Average ppm CO2 from 1981 to 1990 (exclusive)')
+plt.plot(t_axis,y_axis_CO2,'-.')
+plt.show()
 
 # 1b) (SEE PAGE 137)
 # Detrend the 9 year dataset by fitting a low order polynomial with
